cae8aea5a39382dad7c723c5c04c8ecb8ed22533
Adarsh Pandey <meadarshpandey@gmail.com>
2026-06-07 20:46:27 +0530

    ss

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try{
-
+    try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB Connected");
-    } catch (error){
-        console.log(error.message);
+
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error("Mongo Error:", error);
         process.exit(1);
     }
-}
+};
 
 module.exports = connectDB;
