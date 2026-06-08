cb63cd57787cf51478036dd6f38255a36f8a4c4b
Adarsh Pandey <adarsh.pandey@finhaat.in>
2026-06-08 10:51:01 +0530

    updated code

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error("Mongo Error:", error);
-        process.exit(1);
-    }
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("Mongo Error:", error);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
